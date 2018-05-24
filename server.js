884e20c26d1df04f6b37026b2824fd83b7bac4c3
Natchiaporn <35389711+Natchiaporn@users.noreply.github.com>
2018-05-24 16:16:10 +0700

    59160489

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+let contacts = [{
+    name : 'Bell',
+    phonnumber : '0917174201'
+
+}]
+
+app.use(bodyParser.json())
+app.use(cors())
+
+/// TODO: Develop GET /contacts
+
+
+
+/// TODO: Develop POST /contacts
+
+
+
+app.listen(3000, () => {
+    console.log('API Server started at port 3000')
+})
